601e848327a7693cddf252a3aa1a64f9c92649b2
HeberthDev <heberthdeveloper@gmail.com>
2025-02-10 07:58:07 -0300

    Formulário com FormBuilder

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -15,12 +16,20 @@ export class CriarPensamentoComponent implements OnInit {
     modelo: 'modelo1',
   }
 
+  public formulario!: FormGroup;
+
   constructor(
     private pensamentoService: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: [''],
+      modelo: ['modelo1']
+    })
   }
 
   public criarPensamento() {
