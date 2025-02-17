03f44b4d041811c2df536e7a09f77f545c560c49
HeberthDev <heberthdeveloper@gmail.com>
2025-02-17 08:19:11 -0300

    Adicionando ícone de favorito

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -11,7 +11,8 @@ export class PensamentoComponent implements OnInit {
     id: '',
     conteudo: 'I love Angular',
     autoria: 'Nay',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false,
   }
 
   constructor() { }
@@ -24,6 +25,14 @@ export class PensamentoComponent implements OnInit {
     }
 
     return 'pensamento-p';
+  }
+
+  public mudarIconeFavorito(): string {
+    if (this.pensamento.favorito == false) {
+      return 'inativo';
+    }
+
+    return 'ativo';
   }
 
 }
